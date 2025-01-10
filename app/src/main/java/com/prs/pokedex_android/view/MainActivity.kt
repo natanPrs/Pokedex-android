b0074a742ff9824694e692a15fb1df1f782250c0
Natan Pires <natan.void@hotmail.com>
2025-01-10 17:46:53 -0300

    feat: add viewModel

--- app/src/main/java/com/prs/pokedex_android/view/MainActivity.kt
+++ app/src/main/java/com/prs/pokedex_android/view/MainActivity.kt
@@ -1,78 +1,38 @@
 package com.prs.pokedex_android.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.prs.pokedex_android.R
-import com.prs.pokedex_android.api.PokemonRepository
 import com.prs.pokedex_android.domain.Pokemon
+import com.prs.pokedex_android.viewmodel.PokemonViewModel
+import com.prs.pokedex_android.viewmodel.PokemonViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var recyclerView: RecyclerView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-
-        recyclerView = findViewById<RecyclerView>(R.id.rvPokemons)
-
-//        val Charmander = Pokemon(
-//            "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png",
-//            4,
-//            "Charmander",
-//            listOf(PokemonType("Fire"))
-//        )
-//
-//        val pokemons = listOf(Charmander, Charmander, Charmander)
-
-        Thread(Runnable {
-            loadPokemons()
-
-        }).start()
-
+    private val recyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rvPokemons)
     }
 
-    private fun loadPokemons() {
-        val pokemonApiResult = PokemonRepository.listPokemons()
+    private val viewModel by lazy {
+        ViewModelProvider(this, PokemonViewModelFactory())
+            .get(PokemonViewModel::class.java)
+    }
 
-        pokemonApiResult?.results?.let {
-            val pokemon: List<Pokemon?> = it.map { pokemonResult ->
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-                val number = pokemonResult.url
-                    .replace("https://pokeapi.co/api/v2/pokemon/", "")
-                    .replace("/", "").toInt()
+        viewModel.pokemons.observe(this, Observer {
+            loadRecyclerView(it)
+        })
+    }
 
-                val pokemonApiResult = PokemonRepository.getPokemon(number)
-
-                pokemonApiResult?.let {
-                    Pokemon(
-                        pokemonApiResult.id,
-                        pokemonApiResult.name,
-                        pokemonApiResult.types.map { type ->
-                            type.type
-                        }
-                    )
-                }
-
-            }
-
-            val layoutManager = LinearLayoutManager(this)
-
-            recyclerView.post {
-                recyclerView.layoutManager = layoutManager
-                recyclerView.adapter = PokemonAdapter(pokemon)
-            }
-
-        }
+    private fun loadRecyclerView(pokemons: List<Pokemon?>) {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = PokemonAdapter(pokemons)
     }
 }
